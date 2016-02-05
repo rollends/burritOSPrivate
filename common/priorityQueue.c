8858d9370d33f5bdfa4d0fc3b689c7760924cc83
Taylor Petrick <tpetrick@uwaterloo.ca>
2016-02-05 13:08:09 -0500

    Cleaned up pqueue

--- common/priorityQueue.c
+++ common/priorityQueue.c
@@ -3,50 +3,60 @@
 
 S32 priorityQueueInit(PriorityQueue* queue)
 {
-    U8 i = 0;
-    for(i = 0; i < 32; ++i)
+    U8 i;
+    for (i = 0; i < PRIORITY_COUNT; i++)
     {
         queue->head[i] = queue->tail[i] = 0;
     }
-    queue->isNonEmpty = 0;
+
+    queue->priorityBits = 0;
+
     return 0;
 }
 
 S32 priorityQueuePush(PriorityQueue* queue, const U8 priority, const U16 value)
 {
-    queue->data[queue->tail[priority] + priority * 8] = value;
-    queue->tail[priority] = (queue->tail[priority] + 1) & ( 8 - 1);
-    queue->isNonEmpty |= (1 << priority);
+    queue->data[queue->tail[priority] + priority * PRIORITY_LENGTH] = value;
+    queue->tail[priority] = (queue->tail[priority] + 1) & (PRIORITY_LENGTH - 1);
+    queue->priorityBits |= (1 << priority);
+
     return 0;
 }
 
 S32 priorityQueuePop(PriorityQueue* queue, U16* dest)
 {
-    if( 0 == queue->isNonEmpty )
+    if (queue->priorityBits == 0)
+    {
         return -1;
+    }
 
-    U32 priority = __ctz( queue->isNonEmpty );
-    *dest = queue->data[queue->head[priority] + priority * 8];
-    queue->head[priority] = (queue->head[priority] + 1) & (8 - 1);
-    queue->isNonEmpty ^= (queue->head[priority] == queue->tail[priority] ? 1 : 0) << (priority);
+    U32 priority = __ctz(queue->priorityBits);
+
+    *dest = queue->data[queue->head[priority] + priority * PRIORITY_LENGTH];
+    queue->head[priority] = (queue->head[priority] + 1) & (PRIORITY_LENGTH - 1);
+    queue->priorityBits ^=
+        (queue->head[priority] == queue->tail[priority] ? 1 : 0) << (priority);
 
     return 0;
 }
 
 S32 priorityQueuePushPop(PriorityQueue* queue, const U8 priority, U16* value)
 {
-    U32 freePriority = __ctz( queue->isNonEmpty );
+    U32 free = __ctz(queue->priorityBits);
 
-    if (priority < freePriority || 0 == queue->isNonEmpty)
+    if (priority < free)
+    {
         return 1;
+    }
 
-    queue->data[queue->tail[priority] + priority * 8] = *value;
-    queue->tail[priority] = (queue->tail[priority] + 1) & (8-1);
-    queue->isNonEmpty |= (1 << (priority));
+    queue->data[queue->tail[priority] + priority * PRIORITY_LENGTH] = *value;
+    queue->tail[priority] = (queue->tail[priority] + 1) & (PRIORITY_LENGTH - 1);
+    queue->priorityBits |= (1 << (priority));
 
-    *value = queue->data[queue->head[freePriority] + freePriority * 8];
-    queue->head[freePriority] = (queue->head[freePriority] + 1) & (8-1);
-    queue->isNonEmpty ^=
-        (queue->head[freePriority] == queue->tail[freePriority] ? 1 : 0) << (freePriority);
+    *value = queue->data[queue->head[free] + free * PRIORITY_LENGTH];
+    queue->head[free] = (queue->head[free] + 1) & (PRIORITY_LENGTH - 1);
+    queue->priorityBits ^=
+        (queue->head[free] == queue->tail[free] ? 1 : 0) << (free);
+        
     return 0;
 }
