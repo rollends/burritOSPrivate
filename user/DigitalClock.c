6b0635771d69f2922de220aab4ada7f97fb04a41
Rollen Sandeep D'Souza <rs2dsouza@uwaterloo.ca>
2016-02-18 12:28:15 -0500

    Misc. User Tasks for K4
    Changes:
     - Performance task by default now polls ever 300ms for Idle Task
       usage. Maybe increase that timeslice for later. If you want the
       old Kernel Perf information (all tasks) then just use -DKERNEL_PERF_VERBOSE
       flag during build.
     - Digital Clock prints every 10ms the time passed at the HOme location
       of terminal.

---
+++ user/DigitalClock.c
@@ -0,0 +1,27 @@
+#include "user/services/services.h"
+
+void DigitalClock(void)
+{
+    TaskID  clock   = nsWhoIs(Clock),
+            output  = nsWhoIs(TerminalOutput);
+    U16     minutes = 0;
+    U16     seconds = 0;
+    U16     millis  = 0;
+    for(;;)
+    {
+        clockDelayBy(clock, 1);
+        
+        millis += 10;
+        seconds += (millis / 1000);
+        minutes += (seconds / 60);
+
+        millis = millis % 1000;
+        seconds = seconds % 60;
+        
+        printf( output,
+                "\033[s\033[H%2d:%2d.%2d\033[u", 
+                minutes,
+                seconds,
+                millis / 10 );
+    }
+}
