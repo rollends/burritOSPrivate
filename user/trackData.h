fdc2c84ca0e69ade949940ba227afc1fa98ee6a4
Rollen D'Souza <rollensdsouza@gmail.com>
2016-03-05 17:59:18 -0500

    Track Data from Online CS452.  - Track data renamed/reorganized into our naming convention

---
+++ user/trackData.h
@@ -0,0 +1,9 @@
+/* THIS FILE IS GENERATED CODE -- DO NOT EDIT */
+
+#include "trackNode.h"
+
+// The track initialization functions expect an array of this size.
+#define TRACK_MAX 144
+
+void init_tracka(TrackNode *track);
+void init_trackb(TrackNode *track);
---
+++ user/trackNode.h
@@ -0,0 +1,36 @@
+#ifndef TRACK_NODE_H
+#define TRACK_NODE_H
+
+typedef enum {
+    eNodeNone,
+    eNodeSensor,
+    eNodeBranch,
+    eNodeMerge,
+    eNodeEnter,
+    eNodeExit,
+} TrackNodeType;
+
+#define DIR_AHEAD       0
+#define DIR_STRAIGHT    0
+#define DIR_CURVED      1
+
+struct TrackNode;
+typedef struct TrackNode TrackNode;
+typedef struct TrackEdge TrackEdge;
+
+struct TrackEdge {
+    TrackEdge*  reverse;
+    TrackNode*  src; 
+    TrackNode*  dest;
+    int         dist;       /* in millimetres */
+};
+
+struct TrackNode {
+  const char *  name;
+  TrackNodeType type;
+  int           num;        /* sensor or switch number */
+  TrackNode*    reverse;    /* same location, but opposite direction */
+  TrackEdge     edge[2];
+};
+
+#endif
